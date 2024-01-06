f2258480c4f444534ac0e00055f3ff90d59c0976
Diego Cardona <diegocp031293@gmail.com>
2024-01-06 16:01:05 -0500

    test storage response API

--- api/app/views.py
+++ api/app/views.py
@@ -7,10 +7,12 @@
 """
 from .models import PredictionRequest
 from .util import get_model, transform_to_dataframe
+from google.cloud import storage
 import pandas as pd
 import os
 
 model = get_model()
+client = storage.Client()
 
 def get_prediction(request: PredictionRequest) -> float:
     data_to_predict = transform_to_dataframe(request)
@@ -31,11 +33,19 @@ def save_new_data(data_user: pd.DataFrame, prediction: float) -> None:
 
     new_data['prediction'] = prediction
 
-    csv_filename = './dataset/data_storage.csv'
+    # csv_filename = './dataset/data_storage.csv'
+    gcs_bucket_name = 'model-dataset-tracker-abi'
+    gcs_filename = 'data_storage.csv'
 
-    # print('prueba', os.path.exists('./dataset'))
+    # csv_data = new_data.to_csv(index=False)
 
-    if not os.path.exists(csv_filename):
-        new_data.to_csv(csv_filename, index=False, header=True, mode='w')
-    else:
-        new_data.to_csv(csv_filename, index=False, header=False, mode='a')
+    bucket = client.get_bucket(gcs_bucket_name)
+    blob = bucket.blob(gcs_filename)
+    blob.upload_from_string(new_data, content_type='text/csv')
+
+    # if not os.path.exists(csv_filename):
+    #     new_data.to_csv(csv_filename, index=False, header=True, mode='w')
+    #     blob.upload_from_string(new_data, content_type='text/csv')
+    # else:
+    #     new_data.to_csv(csv_filename, index=False, header=False, mode='a')
+    #     blob.upload_from_string(new_data, content_type='text/csv')
